d2950dfb30cc3d2b9b31b31d9ebe2d5ab5ebd413
eli <errpoulos@gmail.com>
2019-03-04 21:53:56 -0500

    Set font

--- pytext.py
+++ pytext.py
@@ -5,19 +5,18 @@ class PyText:
     def __init__(self, master):
         # Default title
         master.title("Untitled-pytext")
-
         # Default canvas size
         master.geometry("1200x700")
+        # set font
+        font_specs   = ("ubuntu", 18)
+
 
         # initialize textarea
-        self.textarea = tk.Text(master)
-
+        self.textarea = tk.Text(master, font = font_specs)
         # initialize scrollbar
         self.scroll = tk.Scrollbar(master, command=self.textarea.yview())
-
         # initialize vertical scroll
         self.textarea.configure(yscrollcommand=self.scroll.set)
-
         # constrain textarea and scroll to size of widget
         self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
