b7cc6fde39436e44c9465af082c30561b34bfe39
eli <errpoulos@gmail.com>
2019-03-04 22:03:42 -0500

    Implemented menubar and file dropdown

--- pytext.py
+++ pytext.py
@@ -1,6 +1,23 @@
 import tkinter as tk
 
 
+class Menubar:
+    def __init__(self, parent):
+        font_specs = ("ubuntu", 14)
+
+        #allow menubar to access PyText class methods
+        menubar = tk.Menu(parent.master,font=font_specs)
+        parent.master.config(menu=menubar)
+
+        file_dropdown = tk.Menu(menubar, font=font_specs)
+        file_dropdown.add_command(label="New File")
+        file_dropdown.add_command(label="Open File")
+        file_dropdown.add_command(label="Save")
+        file_dropdown.add_command(label="Save As")
+        file_dropdown.add_separator()
+        file_dropdown.add_command(label="Exit")
+
+        menubar.add_cascade(label = "File")
 class PyText:
     def __init__(self, master):
         # Default title
@@ -8,11 +25,11 @@ class PyText:
         # Default canvas size
         master.geometry("1200x700")
         # set font
-        font_specs   = ("ubuntu", 18)
-
+        font_specs = ("ubuntu", 18)
+        self.master = master
 
         # initialize textarea
-        self.textarea = tk.Text(master, font = font_specs)
+        self.textarea = tk.Text(master, font=font_specs)
         # initialize scrollbar
         self.scroll = tk.Scrollbar(master, command=self.textarea.yview())
         # initialize vertical scroll
@@ -20,6 +37,8 @@ class PyText:
         # constrain textarea and scroll to size of widget
         self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.menubar = Menubar(self)
 
 
 if __name__ == "__main__":
