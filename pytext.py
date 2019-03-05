e084c8fb24a125964e9e9a44374f97b498bdfb64
eli <errpoulos@gmail.com>
2019-03-04 21:48:37 -0500

    Added textarea, scrollbars, geom constraints

--- pytext.py
+++ pytext.py
@@ -1,12 +1,29 @@
 import tkinter as tk
 
+
 class PyText:
     def __init__(self, master):
+        # Default title
         master.title("Untitled-pytext")
-        master.geometry("1200x700")
-       
 
-if __name__== "__main__":
+        #Default canvas size
+        master.geometry("1200x700")
+
+        #initialize textarea
+        self.textarea = tk.Text(master)
+
+        # initialize scrollbar
+        self.scroll = tk.Scrollbar(master, command=self.textarea.yview())
+
+        # initialize vertical scroll
+        self.textarea.configure(yscrollcommand=self.scroll.set)
+
+        # constrain textarea and scroll to size of widget
+        self.textarea.pack(side=tk.LEFT)
+        self.scroll.pack(side=tk.RIGHT)
+
+
+if __name__ == "__main__":
     master = tk.Tk()
-    pt=PyText(master)
+    pt = PyText(master)
     master.mainloop()
