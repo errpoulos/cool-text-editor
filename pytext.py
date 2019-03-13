d47d098e5112870719f2ad55229cb5cb8da0a6fc
eli <errpoulos@gmail.com>
2019-03-12 22:07:38 -0400

    Finished project

--- pytext.py
+++ pytext.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
 
 class Menubar:
+
     def __init__(self, parent):
         font_specs = ("ubuntu", 14)
 
@@ -10,39 +12,84 @@ class Menubar:
         parent.master.config(menu=menubar)
 
         file_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
-        file_dropdown.add_command(label="New File", command=parent.set_window_title)
-        file_dropdown.add_command(label="Open File", command=parent.open_file)
-        file_dropdown.add_command(label="Save", command=parent.save)
-        file_dropdown.add_command(label="Save As", command=parent.save_as)
+        file_dropdown.add_command(label="New File",
+                                  accelerator="Ctrl+N",
+                                  command=parent.new_file)
+        file_dropdown.add_command(label="Open File",
+                                  accelerator="Ctrl+O",
+                                  command=parent.open_file)
+        file_dropdown.add_command(label="Save",
+                                  accelerator="Ctrl+S",
+                                  command=parent.save)
+        file_dropdown.add_command(label="Save As",
+                                  accelerator="Ctrl+Shift+S",
+                                  command=parent.save_as)
         file_dropdown.add_separator()
-        file_dropdown.add_command(label="Exit")
+        file_dropdown.add_command(label="Exit",
+                                  command=parent.master.destroy)
+
+        about_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
+        about_dropdown.add_command(label="Release Notes",
+                                   command=self.show_release_notes)
+        about_dropdown.add_separator()
+        about_dropdown.add_command(label="About",
+                                   command=self.show_about_message)
 
         menubar.add_cascade(label="File", menu=file_dropdown)
+        menubar.add_cascade(label="About", menu=about_dropdown)
+
+    def show_about_message(self):
+        box_title = "About PyText"
+        box_message = "A simple Python Text Editor"
+        messagebox.showinfo(box_title, box_message)
+
+    def show_release_notes(self):
+        box_title = "Release Notes"
+        box_message = "Version 0.1 - Gutenberg"
+        messagebox.showinfo(box_title, box_message)
+
+
+class Statusbar:
+
+    def __init__(self, parent):
+
+        font_specs = ("ubuntu", 12)
+
+        self.status = tk.StringVar()
+        self.status.set("PyText - 0.1 Gutenberg")
+
+        label = tk.Label(parent.textarea, textvariable=self.status, fg="black",
+                         bg="lightgrey", anchor='sw', font=font_specs)
+        label.pack(side=tk.BOTTOM, fill=tk.BOTH)
+
+    def update_status(self, *args):
+        if isinstance(args[0], bool):
+            self.status.set("Your File Has Been Saved!")
+        else:
+            self.status.set("PyText - 0.1 Gutenberg")
 
 
 class PyText:
-    def __init__(self, master):
-        # Default title
-        master.title("Untitled-pytext")
-        # Default canvas size
-        master.geometry("1200x700")
-        # set font
-        font_specs = ("ubuntu", 18)
-        self.master = master
 
+    def __init__(self, master):
+        master.title("Untitled - PyText")
+        master.geometry("1200x700")
+
+        font_specs = ("ubuntu", 18)
+
+        self.master = master
         self.filename = None
 
-        # initialize textarea
         self.textarea = tk.Text(master, font=font_specs)
-        # initialize scrollbar
-        self.scroll = tk.Scrollbar(master, command=self.textarea.yview())
-        # initialize vertical scroll
+        self.scroll = tk.Scrollbar(master, command=self.textarea.yview)
         self.textarea.configure(yscrollcommand=self.scroll.set)
-        # constrain textarea and scroll to size of widget
         self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.menubar = Menubar(self)
+        self.statusbar = Statusbar(self)
+
+        self.bind_shortcuts()
 
     def set_window_title(self, name=None):
         if name:
@@ -50,12 +97,12 @@ class PyText:
         else:
             self.master.title("Untitled - PyText")
 
-    def new_file(self):
+    def new_file(self, *args):
         self.textarea.delete(1.0, tk.END)
         self.filename = None
         self.set_window_title()
 
-    def open_file(self):
+    def open_file(self, *args):
         self.filename = filedialog.askopenfilename(
             defaultextension=".txt",
             filetypes=[("All Files", "*.*"),
@@ -71,21 +118,21 @@ class PyText:
                 self.textarea.insert(1.0, f.read())
             self.set_window_title(self.filename)
 
-    def save(self):
+    def save(self, *args):
         if self.filename:
             try:
                 textarea_content = self.textarea.get(1.0, tk.END)
-                with open(self.filename,"w") as f:
+                with open(self.filename, "w") as f:
                     f.write(textarea_content)
+                self.statusbar.update_status(True)
             except Exception as e:
                 print(e)
-
         else:
             self.save_as()
 
-    def save_as(self):
+    def save_as(self, *args):
         try:
-            new_file = filedialog.asksaveasfile(
+            new_file = filedialog.asksaveasfilename(
                 initialfile="Untitled.txt",
                 defaultextension=".txt",
                 filetypes=[("All Files", "*.*"),
@@ -96,12 +143,20 @@ class PyText:
                            ("HTML Documents", "*.html"),
                            ("CSS Documents", "*.css")])
             textarea_content = self.textarea.get(1.0, tk.END)
-            with open(new_file, "W") as f:
+            with open(new_file, "w") as f:
                 f.write(textarea_content)
             self.filename = new_file
-
+            self.set_window_title(self.filename)
+            self.statusbar.update_status(True)
         except Exception as e:
             print(e)
+
+    def bind_shortcuts(self):
+        self.textarea.bind('<Control-n>', self.new_file)
+        self.textarea.bind('<Control-o>', self.open_file)
+        self.textarea.bind('<Control-s>', self.save)
+        self.textarea.bind('<Control-S>', self.save_as)
+        self.textarea.bind('<Key>', self.statusbar.update_status)
 
 
 if __name__ == "__main__":
